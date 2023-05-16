e23529814ba4baaf63cc8f0a2a5ae250985c0ed0
Laith Hisham <lahisham@microsoft.com>
2023-05-16 19:36:28 +0300

    add repro files

---
+++ SimpleInjectorDisposeReproHostedService/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SimpleInjector;
+using SimpleInjectorDisposeRepro;
+
+//var builder = WebApplication.CreateBuilder(args);
+var container = new Container();
+
+var host = Host.CreateDefaultBuilder()
+                .ConfigureServices(ConfigureServices)
+                .UseConsoleLifetime()
+                .Build()
+                .UseSimpleInjector(container);
+container.Verify();
+
+var testDisposable = container.GetInstance<TestDisposableAsyncClass>();
+Console.WriteLine(testDisposable.TestMethod());
+
+var cancellationToken = new CancellationTokenSource();
+
+await host.RunAsync(cancellationToken.Token);
+
+void ConfigureServices(IServiceCollection services)
+{
+    services.AddSimpleInjector(container);
+    container.RegisterSingleton<TestDisposableAsyncClass, TestDisposableAsyncClass>();
+}
+
---
+++ SimpleInjectorDisposeReproHostedService/TestDisposableAsyncClass.cs
@@ -0,0 +1,20 @@
+﻿namespace SimpleInjectorDisposeRepro
+{
+    public class TestDisposableAsyncClass : IAsyncDisposable
+    {
+        public bool Disposed { get; set; }
+
+        public async ValueTask DisposeAsync()
+        {
+            Disposed = true;
+            Console.WriteLine("TestDisposableAsyncClass - DisposeAsync called");
+
+            await Task.CompletedTask;
+        }
+
+        public string TestMethod()
+        {
+            return "Test";
+        }
+    }
+}
